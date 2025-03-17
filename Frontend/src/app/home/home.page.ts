1f168ad36a18790c453bba853aeefce76ce55756
LuisJURU <98226251+LuisJURU@users.noreply.github.com>
2025-03-17 18:37:21 -0400

    okay vamos paso a paso

--- Frontend/src/app/home/home.page.ts
+++ Frontend/src/app/home/home.page.ts
@@ -21,7 +21,7 @@ export class HomePage implements OnInit {
   categories: any[] = [];
   selectedCategory: string = '';
   currentPage: number = 1;
-  private apiUrl = 'http://localhost:5000/api/movies'; // URL base del backend
+  private apiUrl = 'https://backend-4hqancgim-luis-jarabas-projects.vercel.app/api/movies'; // URL base del backend en Vercel
 
   constructor(private navCtrl: NavController, private http: HttpClient) { }
 
