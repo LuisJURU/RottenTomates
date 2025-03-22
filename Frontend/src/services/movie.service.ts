69f2dfd76ce9eb76515e5dd45d474947955695ba
LuisJURU <98226251+LuisJURU@users.noreply.github.com>
2025-03-21 21:58:35 -0400

    .

--- Frontend/src/services/movie.service.ts
+++ Frontend/src/services/movie.service.ts
@@ -83,7 +83,7 @@ export class MovieService {
 
   rateMovie(movieId: string, rating: number, comment: string): Observable<any> {
     const payload = { movieId, rating, comment };
-    const apiUrl = 'https://rotten-tomates-git-main-luis-jarabas-projects.vercel.app/api/movie/rate';
+    const apiUrl = 'https://rotten-tomates-git-main-luis-jarabas-projects.vercel.app/api/movies/rate';
     return from(axios.post(apiUrl, payload).then(response => response.data));
   }
 }
